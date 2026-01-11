ab16743c1811272673177141552809573b52e710
Gustaf Mossakowski <gustaf@koenige.org>
2026-01-11 14:58:36 +0100

    format: add functions to convert PGN date to ISO 8601 date, to convert to localized date, for use instead of parse_date()

--- chess/format.inc.php
+++ chess/format.inc.php
@@ -22,3 +22,25 @@
 function mf_chess_pgn_wordwrap($string) {
 	return wordwrap($string, 79);
 }
+
+/**
+ * convert a PGN date into an ISO 8601 date
+ *
+ * @param string $date
+ * @return string
+ */
+function mf_chess_pgn_date($date) {
+	$iso = str_replace('.', '-', $date);
+	$iso = str_replace('??', '00', $iso);
+	return $iso;
+}
+
+/**
+ * convert a PGN date into an ISO date
+ *
+ * @param string $date
+ * @return string
+ */
+function mf_chess_pgn_date_localized($date) {
+	return wrap_date(mf_chess_pgn_date($date));
+}
